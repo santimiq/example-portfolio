9e23d56b0bdb873e17541c014a18a90bc2b73ff5
Santiago Miqueo <smiqueo@hotmail.com>
2020-05-07 17:21:28 -0500

    Deleted layout css, js and seo and modified some pages, also add folder styles with styles.scss

--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -2,16 +2,10 @@
 import React from "react"
 import { PageProps, Link } from "gatsby"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
-const SecondPage = (props: PageProps) => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2 ({props.path})</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+const SecondPage = () => (
+  <p>
+    2 page
+  </p>
 )
 
 export default SecondPage
